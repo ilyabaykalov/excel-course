7f6a86e1025a3ce982524d48ee5f835f7179f488
Ilya Baykalov <enot_boris@icloud.com>
2020-06-15 19:07:45 +0700

    Добавлен класс для рабоыт в DOM

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,21 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 
 export class Table extends ExcelComponent {
+  static className = 'excel__table';
 
+  toHTML() {
+    return `
+      <div class='row'>
+        <div class='row-info'></div>
+        <div class='row-data'>
+            <div class='column'>A</div>
+        </div>
+      </div>
+      <div class='row'>
+        <div class='row-info'>1</div>
+        <div class='row-data'>
+            <div class='cell' contenteditable>A1</div>
+        </div>
+      </div>`;
+  }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,6 +1,7 @@
 import { DOMListener } from '@core/DOMListener';
 
 export class ExcelComponent extends DOMListener {
+  // Возвращает шаблон компонента
   toHTML() {
     return '';
   }
